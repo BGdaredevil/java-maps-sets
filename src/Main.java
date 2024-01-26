7cbdeab0857c43ebb41276cb3e41ffa7570e328e
dimitar.dimitrov <dimitar.dimitrov@sirma.bg>
2024-01-26 13:44:14 +0200

    solved grades

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
-import java.util.Arrays;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -9,7 +7,37 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("input");
-        countRealNums(sc);
+
+//        countRealNums(sc);
+        getStudentAverage(sc);
+
+    }
+
+    public static void getStudentAverage(Scanner sc) {
+        int size = Integer.parseInt(sc.nextLine());
+        Map<String, ArrayList<Double>> studentsList = new TreeMap<>();
+
+        while (size > 0) {
+            size--;
+            String[] input = sc.nextLine().split(" ");
+            String personName = input[0];
+            double personGrade = Double.parseDouble(input[1]);
+            if (!studentsList.containsKey(personName)) {
+                studentsList.put(personName, new ArrayList<>(1));
+            }
+
+            studentsList.get(personName).add(personGrade);
+        }
+
+        studentsList.forEach((studentName, gradesList) -> {
+            Optional<Double> avgGradeOpt = gradesList.stream().reduce(Double::sum);
+            if (avgGradeOpt.isPresent()) {
+                double averageGrade = avgGradeOpt.get() / gradesList.size();
+                String grades = gradesList.stream().map(e -> String.format("%.2f", e)).collect(Collectors.joining(" "));
+
+                System.out.printf("%s -> %s (avg: %.2f)\n", studentName, grades, averageGrade);
+            }
+        });
 
     }
 
